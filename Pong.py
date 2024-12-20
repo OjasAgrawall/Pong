4cf0d23f351e22ceb131e3dc5ad2a9271e35ef8d
ojase <ojas.e.agrawal@gmail.com>
2024-12-19 22:35:55 -0500

    added the centre line!

--- Pong.py
+++ Pong.py
@@ -2,10 +2,12 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+TEMP_SCREEN_WIDTH = 800
+TEMP_SCREEN_HEIGHT = 600
+NAME = "Pong"
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), flags = pygame.RESIZABLE)
+screen = pygame.display.set_mode((TEMP_SCREEN_WIDTH, TEMP_SCREEN_HEIGHT), flags = pygame.RESIZABLE)
+pygame.display.set_caption(NAME)
 
 
 #Colours
@@ -24,6 +26,7 @@ player2X = screen.get_width() - 100
 player2Y = 0
 
 def paddleMovement():
+    global player1Y, player2Y
     key = pygame.key.get_pressed()
     if key[pygame.K_w] == True:
         player1Y -= PADDLE_MOVE_SPEED
@@ -56,6 +59,13 @@ def drawPaddles():
     pygame.draw.rect(screen, WHITE, player1)
     pygame.draw.rect(screen, WHITE, player2)
 
+def drawCentreLine():
+    SCREEN_MID = screen.get_width() / 2
+    SCREEN_HEIGHT = screen.get_height()
+    for y in range(0, SCREEN_HEIGHT, SCREEN_HEIGHT // 10):
+        pygame.draw.rect(screen, WHITE, (SCREEN_MID - 2, y + 20, 4, (SCREEN_HEIGHT//20)))
+
+
 TPS = 100
 delay = int(1000 / TPS)
 
@@ -77,6 +87,9 @@ while run:
     #actually draw the paddles onto the screen
     drawPaddles()
 
+    drawCentreLine()
+
+    #if you want to leave you can
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
